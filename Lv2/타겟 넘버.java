f55862c7431d62e5afe2977ada9044c5f4a1a9e6
KwakMJ <kwak2002mj@gmail.com>
2024-09-12 15:24:12 +0900

    ♻️refactor: [Lv2] 타겟 넘버
    DFS : 스택, 재귀 두 방법으로 문제 해결
    BFS로 하니 시간 초과가 나서 BFS는 사용하지 않았음

--- Lv2/타겟 넘버.java
+++ Lv2/타겟 넘버.java
@@ -1,43 +1,82 @@
+// 1. DFS 코드 (Stack 사용)
+
 import java.util.*;
 
 class Solution {
+    static class Node {
+        int data;
+        int idx;
+        
+        Node(int data, int idx) {
+            this.data = data;
+            this.idx = idx;
+        }
+    }
+    
     public int solution(int[] numbers, int target) {
         int answer = 0;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        answer = DFS(numbers, target);
         
-        int tmp = 0;
-        for(int i: numbers) {
-            if(map.containsKey(i)) map.put(i, map.get(i)+1);
-            else map.put(i, 1);
-            tmp += i;
-        }
-        if(tmp == target) {
-            answer = 1;
-            return answer;
-        }
-        numbers = Arrays.sort(numbers);
-        boolean[] visited = new boolean[numbers[number.length-1]];
-        
-        for(int i=0; i<numbers.length-1; i++) {
-            int rest=target-numbers[i];
-            visited[numbers[i]] = true;
-            
-            
-            for(int j=0; j<numbers.length; j++) {
-                
-                for(int k=0; k<numbers.length; k++) {
-                    if(!visited[numbers[j]]) {
-                        if()
-                    } else {
-                        tmp += numbers
-                    }
-                }
-                
-                
+        return answer;
+    }
+    
+    public static int DFS(int[] numbers, int target) {
+        int answer = 0;
+        Stack<Node> s = new Stack<>();
+        s.push(new Node(numbers[0], 0));
+        s.push(new Node(-numbers[0], 0));
+        while(!s.isEmpty()) {
+            Node curr = s.pop();
+            if(curr.idx >= numbers.length-1) {
+                if(curr.data == target) answer ++;
+                continue;
             }
+            
+            int pos = curr.data+numbers[curr.idx+1];
+            int neg = curr.data-numbers[curr.idx+1];
+            s.push(new Node(pos, curr.idx+1));
+            s.push(new Node(neg, curr.idx+1));
         }
         
         return answer;
+    }
+}
+
+
+// 2. DFS 코드 (재귀 사용)
+import java.util.*;
+
+class Solution {
+    static int answer;
+    static int target;
+    static int[] numbers;
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        Solution.numbers = numbers;
+        Solution.target = target;
+        
+        DFS(numbers[0], 0);
+        DFS(-numbers[0], 0);
+        
+        return answer;
+    }
+    
+    public static void DFS(int data, int idx) {
+        
+        int pos = data+numbers[idx+1];
+        int neg = data-numbers[idx+1];
+        
+        if(idx+1 >= numbers.length-1) {
+            if(pos == target) answer++;
+            if(neg == target) answer++;
+            
+            return;
+        }
+        
+        DFS(pos, idx+1);
+        DFS(neg, idx+1);
+        
     }
 }
