c41dc4830fdf18b668954a4a7b816a20c56138aa
KwakMJ <kwak2002mj@gmail.com>
2024-09-28 23:43:21 +0900

    ♻️refactor: [Lv2] 요격 시스템

--- Lv2/요격 시스템.java
+++ Lv2/요격 시스템.java
@@ -3,24 +3,14 @@ class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
         
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for(int i=0; i<targets.length; i++) {
-            int a = targets[i][0];
-            int b = targets[i][1];
-            for(int j=a+1; j<=b; j++) {
-                if(map.containsKey(j)) {
-                    map.get(j).add(i);
-                    map.put(j, map.get(j));
-                } else {
-                    Set<Integer> set = new HashSet<>();
-                    set.add(i);
-                    map.put(j, set);
-                }
-            }
-        }
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
         
-        for(int key: map.keySet()) {
-            
+        int misile = 0;
+        for(int i=0; i<targets.length; i++){
+            if(misile <= targets[i][0]) {
+                misile = targets[i][1];
+                answer ++;
+            }
         }
         
         return answer;
